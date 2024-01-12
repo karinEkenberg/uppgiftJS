2108d66d90f8ac70d255cb0c83a73445c98c43a5
karinEkenberg <137770952+karinEkenberg@users.noreply.github.com>
2024-01-12 11:44:37 +0100

    Update script.js

--- HederligeHarryJS2-main/HederligeHarryJS2-main/script.js
+++ HederligeHarryJS2-main/HederligeHarryJS2-main/script.js
@@ -1,31 +1,3 @@
-/* 1. skapa en script.js som anropas från HTML-filen
-2. Lägg till en eventlyssnare på knappen med id "button-contact"
-som kör en funktion när man klickar på knappen. Funktionen som körs när 
-man klickar på knappen ska läsa texten från text-input:en med id "email" 
-och logga den texten i konsolen med console.log
-3. Skriv en funktion createParagraph som tar emot en text som parameter. 
-Funktionen ska sedan skapa en <p>-tag med den inskickade texten som värde. 
-Funktionen ska sedan returnera den skapade <p>-taggen. 
-4. Varje gång man ÄNDRAR på phone så ska funktionen createParagraph anropas - 
-du skickar med värdet i phone och p-taggen som returneras ska skrivas till console.
-log
-5. Skapa ett PRIVAT repository på Github, alla nödvändiga filer skall pushas dit. 
-Bjud in stefan.holmberg@systementor.se som Collaborator
-6. Skriv in URL till ditt github som svar här i Learnpoint 
-Krav för VG:
-6. Skapa en ny branch som du kallar för VG
-7.  När man ändrar email-fältet: Om man skrivit in en ".se"-address så ska du: 
-skapa/visa ett fält som heter swedishpostalcode med klass bx-code. Annars ska 
-den INTE visas
-8. Skapa en array med 10 bilar - varje bil ska ha bildurl + id  + typ + årtal. 
-Dessa ska renderas så som dom görs idag
-id = löpnummer
-bildurl = det finns ca 100 bilar på
-https://axmjqhyyjpat.objectstorage.eu-amsterdam-1.oci.customer-oci.com/n/axmjqhyyjpat/b/randomimages/o/cars%2F13.png
-osv där 13 på slutet är ett löpnummer
-Pris = sätt till vad du vill*/
-
-
 const contactButton = document.getElementById("button-contact");
 let emailInput = document.getElementById('input-email')
 const phoneInput = document.getElementById("input-phone");
